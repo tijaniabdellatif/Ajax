8cfc757f0e63a148f23894616fca33244d68994f
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-07-12 14:19:50 +0100

    style-side_bar_nav

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,900;1,100;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Messenger - App</title>
+</head>
+
+<body>
+    <nav id="nav">
+
+        nav Area
+
+
+    </nav>
+
+    <div class="chat-container">
+
+        <section id="sidebar">
+
+            <ul class="left-ul">
+                <li><a href=""><span class="profile-img-span"><img src="assets/img/pro1.jpg" alt="Profile image" class="profil-img"></span></a></li>
+                <li><a href="">Tijani Abdellatif<span class="cool-hover"></span></a></li>
+                <li><a href="">Change Name<span class="cool-hover"></span></a></li>
+                <li><a href="">Change Password<span class="cool-hover"></span></a></li>
+                <li><a href="">110 User Online<span class="cool-hover"></span></a></li>
+            </ul>
+
+
+        </section>
+
+        <section id="right-area">
+            <div class="messages">
+
+                message
+
+            </div>
+
+            <div class="chat-form">
+
+                chat form
+
+            </div>
+        </section>
+
+    </div>
+
+
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
