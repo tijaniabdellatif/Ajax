3573d4d8b735775cd9e4107dc303fe578a1011ef
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-07-12 02:22:33 +0100

    begin

---
+++ signup.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,900;1,100;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Create New Account</title>
+</head>
+
+<body>
+
+    <!-- <div class="container">
+    <p>Parent class</p>
+    <div class="child">
+
+    <p>Child Class</p>
+    </div>
+</div> -->
+
+    <!-- <div class="myname">Tijani abdellatif</div> -->
+
+    <!-- <div class="container">
+
+        <div class="item1">
+            <h1>Item1</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+        <div class="item2">
+            <h1>Item2</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+        <div class="item3">
+            <h1>Item3</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+        <div class="item4">
+            <h1>Item4</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+        <div class="item1">
+            <h1>Item1</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+        <div class="item2">
+            <h1>Item2</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+        <div class="item3">
+            <h1>Item3</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+        <div class="item4">
+            <h1>Item4</h1>
+            <p>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.
+                Recusandae ipsa eius amet, repellendus
+                sit delectus autem velit debitis laborum adipisci voluptatibu
+
+            </p>
+        </div>
+
+
+    </div> -->
+
+
+    <div class="sign-up">
+
+        <div class="account-left">
+            <h1>LEFT</h1>
+
+        </div>
+        <!--close accounnt-left container-->
+
+
+        <div class="account-right">
+
+            <div class="form-area">
+
+                <form action="" method="post">
+
+                    <div class="group">
+
+                        <input type="text" name="full_name" class="control" placeholder="Enter Full Name">
+
+
+                    </div>
+                    <div class="group">
+
+                        <input type="email" name="full_email" class="control" placeholder="Enter Your Email">
+
+
+                    </div>
+                    <div class="group">
+
+                        <input type="password" name="passsword" class="control" placeholder="Create password...">
+
+
+                    </div>
+                    <div class="group">
+                        <label for="file" id="file-label">Choose image</label>
+                        <input type="file" name="img" class="file" id="file">
+
+                    </div>
+                    <div class="group">
+
+                        <input type="submit" name="signup" class="btn signup-btn" value="Create Account">
+
+
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!--close acoount  container-->
+
+
+    <script src="assets/js/jquery.min.js"></script>
+</body>
+
+</html>
