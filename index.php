03e29d951332c5efd6840c9bc167ba746eb11e78
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-07-12 15:25:50 +0100

    messages_section

--- index.php
+++ index.php
@@ -35,10 +35,195 @@
 
         <section id="right-area">
             <div class="messages">
+                <div class="left-message common-margin">
+                    <div class="sender-img-block">
 
-                message
+                        <img src="assets/img/pro1.jpg" class="sender-img">
 
+                    </div>
+                    <div class="left-msg-area">
+
+                        <div class="user-name-date">
+                            <span class="sender-name">
+
+                                Tijani Abdellatif
+                            </span>
+
+                            <span class="date-time">1 day ago</span>
+
+                        </div>
+
+                        <div class="left-msg">
+                            <p>Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                Quaerat ducimus odit molestias,
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="left-message common-margin">
+                    <div class="sender-img-block">
+
+                        <img src="assets/img/pro1.jpg" class="sender-img">
+
+                    </div>
+                    <div class="left-msg-area">
+
+                        <div class="user-name-date">
+                            <span class="sender-name">
+
+                                Tijani Abdellatif
+                            </span>
+
+                            <span class="date-time">1 day ago</span>
+
+                        </div>
+
+                        <div class="left-msg">
+                            <p>Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                Quaerat ducimus odit molestias,
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Iusto impedit at sequi quod porro quia qui mollitia,
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="left-message common-margin">
+                    <div class="sender-img-block">
+
+                        <img src="assets/img/pro1.jpg" class="sender-img">
+
+                    </div>
+                    <div class="left-msg-area">
+
+                        <div class="user-name-date">
+                            <span class="sender-name">
+
+                                Tijani Abdellatif
+                            </span>
+
+                            <span class="date-time">1 day ago</span>
+
+                        </div>
+
+                        <div class="left-msg">
+                            <p>Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                Quaerat ducimus odit molestias,
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Iusto impedit at sequi quod porro quia qui mollitia,
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Iusto impedit at sequi quod porro quia qui mollitia,
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Iusto impedit at sequi quod porro quia qui mollitia,
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                                Iusto impedit at sequi quod porro quia qui mollitia,
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="right-messages common-margin">
+                    <div class="right-msg-area">
+
+                        <span class="date-time right-time">
+
+                            1 day ago
+                        </span>
+
+                        <div class="right-msg">
+
+                            <p>Lorem ipsum dolor sit amet
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
+
+
 
             <div class="chat-form">
 
